71ec24d98e22389af0e89c5552746d0b4da8ca6b
Tarandeep100 <tarandeepsingh100@gmail.com>
2022-06-27 02:19:14 -0400

    cors issue

--- patient-data/application/backend/src/server.ts
+++ patient-data/application/backend/src/server.ts
@@ -173,7 +173,35 @@ buildWallet(walletPath).then((_wallet) => {
 const app = express();
 const port = 8080;
 
+const cors = require('cors');
 
+var corsOptions = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }
+
+app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 });
