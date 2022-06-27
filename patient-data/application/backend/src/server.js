436d0d8194a0f5632829f85f16dc7a03bbe9c51c
Tarandeep100 <tarandeepsingh100@gmail.com>
2022-06-27 02:22:48 -0400

    cors issue

--- patient-data/application/backend/src/server.js
+++ patient-data/application/backend/src/server.js
@@ -44,35 +44,6 @@ var express = require("express");
 var app = express();
 var port = 8080; // default port to listen
 // define a route handler for the default home page
-const cors = require('cors');
-
-var corsOptions = {
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  }
-
-app.use(cors(corsOptions));
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 // start the Express server
 app.listen(port, function () {
     console.log("server started at http://localhost:".concat(port));
